bcf7b94864cf0a40366fd3b402ff4e2ec8ea9fec
Amir Kouretchian <amir.kouretchian@gmail.com>
2016-02-23 10:45:32 -0800

    plopping my scratch environment into this repo - initial commit to get project up and running on azure (hopefully.)

---
+++ AllOfAmir/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AllOfAmir
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts")
+                   .Include("~/Scripts/jquery-{version}.js")
+                   .Include("~/Scripts/bootstrap.js"));
+            
+            bundles.Add(new StyleBundle("~/bundles/styles")
+                   .Include("~/Content/bootstrap.css")
+                   .Include("~/Content/site.css"));
+        }
+    }
+}
