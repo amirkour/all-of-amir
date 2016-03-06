a746796011753e2d730cf6adefcc4176ab67ca0d
Amir Kouretchian <amir.kouretchian@gmail.com>
2016-03-06 12:07:41 -0800

    adding DictionaryUtils project to solution - this is just a high-level project to define some APIs for dictionary operations that boggle needs

---
+++ DictionaryUtils/IWordDefiner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryUtils
+{
+    /// <summary>
+    /// Impelementations of this interface know how to perform some word-related operations,
+    /// such as whether or not a string is considered a 'word' or a 'prefix'.
+    /// 
+    /// For now, implementors only need to care about English.
+    /// </summary>
+    public interface IWordDefiner
+    {
+        /// <summary>
+        /// Returns true if the given string is considered a proper prefix.
+        /// 
+        /// For example: "hell" is a proper prefix for "hello"
+        /// 
+        /// Note that a word is not it's own prefix - in other words, "hello" is
+        /// not a prefix for "hello" - in this case, "hello" is an exact word
+        /// (see the API for exact words in this interface.)
+        /// </summary>
+        bool IsPrefix(string prefix);
+
+        /// <summary>
+        /// Returns true if the given string is an exact word, false otherwise.
+        /// 
+        /// An "exact word" is one that has a definition in a dictionary - which
+        /// dictionary?  That's up to the implementor ...
+        /// </summary>
+        bool IsExactWord(string word);
+    }
+}
